7f87f44d423ac2d0fe280a1e9fa923da870f0d79
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-11-02 19:01:10 +1100

    Added ProductList page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 import './App.css';
+
 
 const App = () => {
   return (
-    <Home />
+    <ProductList />
   );
 }
 
