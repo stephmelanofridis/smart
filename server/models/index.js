aa1c7e142d3eea81628235cac9dcf4e8ab6620c6
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-11-03 23:11:57 +1100

    Added git ignore to all folders

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Cart = require('./Cart');
+const Order = require('./Order');
+const Product = require('./Product');
+const User = require('./User');
+
+module.exports = { Cart, Order, Product, User };
