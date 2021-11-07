7a9f145f57c267dc83cf3d9e916cd94f7075c8b4
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-11-07 21:36:30 +1100

    Heroku deployment

--- server/server.js
+++ server/server.js
@@ -15,13 +15,14 @@ app.use(express.json());
 app.use(graphqlUploadExpress());
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 
 async function startApolloServer(typeDefs, resolvers) {
     await server.start();
