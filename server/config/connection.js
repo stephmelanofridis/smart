91696fb538b2cdca848f654c69e1388d63f5f353
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-11-09 15:57:22 +1100

    Changed port number in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/smartdb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/smartdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
