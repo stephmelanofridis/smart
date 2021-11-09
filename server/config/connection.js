e15db1c17c0e23ba55963501addcc3670c617889
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-11-09 23:24:38 +1100

    Enabled procfile in resources on Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,4 +5,14 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/smartdb', {
     useUnifiedTopology: true,
 });
 
+const connection = mongoose.connection;
+
+connection
+    .once('open', () => {
+        console.log('mongoDB database connection established');
+    })
+    .on('error', (err) => {
+        console.log('Error: ', err);
+    });
+
 module.exports = mongoose.connection;
