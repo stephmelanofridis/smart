401733b94068a314bf39e6edd8fd9398898b1484
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-11-09 15:52:37 +1100

    Added port to local host in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/smartdb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/smartdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
