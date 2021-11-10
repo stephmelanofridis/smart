1b2e23d53070dee4aa3709f35eb3a8f1d6d7945c
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-11-10 12:27:28 +1100

    Testing connection

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
-const Category = require('./Category');
 const Order = require('./Order');
 const Product = require('./Product');
 const User = require('./User');
+const Category = require('./Category');
 
 module.exports = { Category, Order, Product, User };
